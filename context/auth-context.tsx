fe2341a141812ca05df072ad7e845270ee8f921b
Aldo Tobing <31465898+aldotobing@users.noreply.github.com>
2025-05-26 00:12:13 +0700

    Update auth-context.tsx

--- context/auth-context.tsx
+++ context/auth-context.tsx
@@ -146,7 +146,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+redirectTo: `${window.location.origin}/`,
+          //redirectTo: `${window.location.origin}/auth/callback`,
         },
       });
 
